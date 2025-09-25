435e3b594cb54e27ff2f7e2880c23287d90f8d15
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-09-25 16:55:27 +0700

    feat: add balance logic

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,8 @@ function App() {
   );
 }
 
+function SplitBillContainer() {}
+
 function StaticContainer({ children }) {
   return (
     <div className="max-w-screen border-2 items-center py-20 mb-5 flex flex-col border-gray-400/10 p-2 m-4 rounded-lg bg-white shadow-xs">
--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -3,7 +3,11 @@ export function Friend({ list }) {
     <div className="p-5 rounded-md flex flex-row mx-5 justify-between bg-gray-400/5 shadow-xs border border-gray-300/30">
       <div className="flex flex-col gap-1">
         <h1>{list.name}</h1>
-        <h1>{list.status}</h1>
+        <h1>
+          {list.balance > 0
+            ? `${list.name} owes you ${list.balance}$`
+            : `You owe ${list.name} ${Math.abs(list.balance)}$`}
+        </h1>
       </div>
       <div>
         <button className="p-3 bg-blue-100 cursor-pointer">Select</button>
